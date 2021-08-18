62790b6513fa6af7c99c9262c09b5052f4f2f625
SWECO\BEGIVL_LA <gillesvandevyver@hotmail.be>
2021-08-18 15:18:46 +0200

    xml generator

---
+++ xml_generator_augmentations.py
@@ -0,0 +1,26 @@
+from shutil import copyfile
+from PIL import Image
+import os
+import PIL
+from tqdm import tqdm
+import gc
+
+# this needs to be executed before flipping
+
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
+directory = 'tennis_data\\train\images'
+
+
+print("flipping images...")
+for filename in tqdm(os.listdir(directory)):
+    f = os.path.join(directory, filename)
+    gc.collect()
+    if os.path.isfile(f):
+        if "aug" in f and not 'hor' in f and not 'ver' in f:
+            target_xml_loc=f[0:18]+'annotations\\'+f[-30:-3]+'xml'
+            source_xml_loc=f[0:18]+'annotations\\'+f[-30:-9]+'.xml'
+            copyfile(source_xml_loc, target_xml_loc)
+        img=None
+
+
+

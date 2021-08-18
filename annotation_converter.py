ea738430f0c25d98de18ddc4679e524a07bca417
SWECO\BEGIVL_LA <gillesvandevyver@hotmail.be>
2021-08-18 14:57:43 +0200

    geometrical augmentor

---
+++ annotation_converter.py
@@ -0,0 +1,50 @@
+from lxml import etree
+
+class CreateAnnotations:
+    # -----Initialization
+    def __init__(self, flodername, filename):
+        self.root = etree.Element("annotation")
+
+        child1 = etree.SubElement(self.root, "folder")
+        child1.text = flodername
+
+        child2 = etree.SubElement(self.root, "filename")
+        child2.text = filename
+
+        child3 = etree.SubElement(self.root, "path")
+        child3.text = filename
+
+        child4 = etree.SubElement(self.root, "source")
+
+        child5 = etree.SubElement(child4, "database")
+        child5.text = "Unknown"
+
+    # -----Set size
+    def set_size(self, imgshape):
+        (height, witdh, channel) = imgshape
+        size = etree.SubElement(self.root, "size")
+        widthn = etree.SubElement(size, "width")
+        widthn.text = str(witdh)
+        heightn = etree.SubElement(size, "height")
+        heightn.text = str(height)
+        channeln = etree.SubElement(size, "depth")
+        channeln.text = str(channel)
+
+    # -----Save the file
+    def savefile(self, filename):
+        tree = etree.ElementTree(self.root)
+        tree.write(filename, pretty_print=True, xml_declaration=False, encoding='utf-8')
+
+    def add_pic_attr(self, label, xmin, ymin, xmax, ymax):
+        object = etree.SubElement(self.root, "object")
+        namen = etree.SubElement(object, "name")
+        namen.text = label
+        bndbox = etree.SubElement(object, "bndbox")
+        xminn = etree.SubElement(bndbox, "xmin")
+        xminn.text = str(xmin)
+        yminn = etree.SubElement(bndbox, "ymin")
+        yminn.text = str(ymin)
+        xmaxn = etree.SubElement(bndbox, "xmax")
+        xmaxn.text = str(xmax)
+        ymaxn = etree.SubElement(bndbox, "ymax")
+        ymaxn.text = str(ymax)
